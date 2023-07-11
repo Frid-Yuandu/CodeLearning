7de9112c00c112bebefec752c44553da2d1eb153
yuandu <friedrich_wong@outlook.com>
2023-07-11 14:22:29 +0800

    Refactor user.ProcessMessage method. Encapsulate search online users and side-text functions as separate methods.

--- Go-IMsystem/user.go
+++ Go-IMsystem/user.go
@@ -67,44 +67,17 @@ func (u *User) SendToUser(msg string) {
 // ProcessMessage implement business of checking & sending message.
 func (u *User) ProcessMessage(msg string) {
 	if msg == "who" {
-		// Searching online user.
-		s := u.server
+		u.SearchOnlineUsers()
 
-		s.onlineMap.Range(func(_, otherUser any) bool {
-			onlineMsg := "[" + otherUser.(*User).Addr + "]" + otherUser.(*User).Name + ":" + "is online.\n"
-			u.SendToUser(onlineMsg)
-			return true
-		})
-
-	} else if len(msg) > 7 && msg[:7] == "rename|" {
+	} else if strings.HasPrefix(msg, "rename|") {
 		// Expected message type: rename|newName. Everything after "|" will be the new
 		// name. strings.Split method will split the message by "|" and returns a slice
 		// of string containing the 2 elements: "rename" and the variable newName.
 		newName := strings.Split(msg, "|")[1]
 		u.Rename(newName)
 
-	} else if len(msg) > 4 && msg[:3] == "to|" {
-		// Expected message type: to|remoteName|msg.
-		splitMsg := strings.Split(msg, "|")
-		if len(splitMsg) != 3 {
-			u.SendToUser("side-text type error, please type in 'to|remoteName|msg'")
-			return
-		}
-
-		remoteName := splitMsg[1]
-		remoteUser, ok := u.server.onlineMap.Load(remoteName)
-		if !ok {
-			u.SendToUser("no such user:" + remoteName)
-			return
-		}
-
-		content := splitMsg[2]
-		if content == "" {
-			u.SendToUser("empty message, please type something.")
-			return
-		}
-		remoteUser.(*User).SendToUser(u.Name + " send to you:" + content)
-
+	} else if strings.HasPrefix(msg, "to|") {
+		u.SideText(msg)
 	} else {
 		u.server.Broadcast(u, msg)
 	}
@@ -123,4 +96,37 @@ func (u *User) ListenMessage() {
 			fmt.Println("raise from User.ListenMessage | user:", u.Name, "conn.Write error:", err)
 		}
 	}
+}
+
+func (u *User) SearchOnlineUsers() {
+	s := u.server
+
+	s.onlineMap.Range(func(_, otherUser any) bool {
+		onlineMsg := "[" + otherUser.(*User).Addr + "]" + otherUser.(*User).Name + ":" + "is online.\n"
+		u.SendToUser(onlineMsg)
+		return true
+	})
+}
+
+func (u *User) SideText(msg string) {
+	// Expected message type: to|remoteName|msg
+	splitMsg := strings.Split(msg, "|")
+	if len(splitMsg) != 3 {
+		u.SendToUser("side-text type error, please type in 'to|remoteName|msg'")
+		return
+	}
+
+	remoteName := splitMsg[1]
+	remoteUser, ok := u.server.onlineMap.Load(remoteName)
+	if !ok {
+		u.SendToUser("no such user:" + remoteName)
+		return
+	}
+
+	content := splitMsg[2]
+	if content == "" {
+		u.SendToUser("empty message, please type something.")
+		return
+	}
+	remoteUser.(*User).SendToUser(u.Name + " send to you:" + content)
 }
