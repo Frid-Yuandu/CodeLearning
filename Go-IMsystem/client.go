78055f0a47d70f54a7ee86f5e29d576551a3bd76
yuandu <friedrich_wong@outlook.com>
2023-07-11 23:13:37 +0800

    Finish client building.

--- Go-IMsystem/client.go
+++ Go-IMsystem/client.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
+	"strings"
 )
 
 var (
@@ -23,7 +26,8 @@ func (c *Client) Menu() bool {
 	var selectFlag int
 	fmt.Println("1.Public chat\n" +
 		"2.Private chat\n" +
-		"3.Rename\n" +
+		"3.Search online users\n" +
+		"4.Rename\n" +
 		"0.Exit")
 
 	_, err := fmt.Scanln(&selectFlag)
@@ -31,7 +35,7 @@ func (c *Client) Menu() bool {
 		fmt.Println("fmt.Scanln error:", err)
 	}
 
-	if selectFlag >= 0 && selectFlag <= 3 {
+	if selectFlag >= 0 && selectFlag <= 4 {
 		c.selectFlag = selectFlag
 		return true
 	}
@@ -47,11 +51,109 @@ func (c *Client) Run() {
 		switch c.selectFlag {
 		case 1:
 			fmt.Println("=====>Public chat<=====")
+			c.PublicChat()
 		case 2:
 			fmt.Println("=====>Private chat<=====")
+			c.PrivateChat()
 		case 3:
+			fmt.Println("=====>Search online users<=====")
+			c.SearchOnlineUsers()
+		case 4:
 			fmt.Println("=====>Rename<=====")
+			c.Rename()
 		}
+	}
+}
+
+func (c *Client) DealResponse() {
+	_, err := io.Copy(os.Stdout, c.conn)
+	if err != nil {
+		fmt.Println("io.Copy error:", err)
+	}
+}
+
+func (c *Client) Rename() bool {
+	fmt.Println(">>>>>please type in new name:")
+	_, _ = fmt.Scanln(&c.Name)
+
+	sendMsg := "rename|" + c.Name + "\n"
+	_, err := c.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("raise from Client.Rename | conn.Write error:", err)
+		return false
+	}
+	return true
+}
+
+func (c *Client) SearchOnlineUsers() {
+	sendMsg := "who\n"
+	_, err := c.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("raise from Client.SearchOnlineUsers | conn.Write error:", err)
+		return
+	}
+}
+
+func (c *Client) PublicChat() {
+	var chatMsg string
+	for strings.ToLower(chatMsg) != "exit" {
+		chatMsg = ""
+		fmt.Println(">>>>>please type in message, exit to exit:")
+		fmt.Scanln(&chatMsg)
+
+		if len(chatMsg) == 0 {
+			fmt.Println("message can't be empty")
+			continue
+		}
+
+		sendMsg := chatMsg + "\n"
+		_, err := c.conn.Write([]byte(sendMsg))
+		if err != nil {
+			fmt.Println("raise from Client.PublicChat | conn.Write error:", err)
+			return
+		}
+	}
+}
+
+func (c *Client) PrivateChat() {
+	var chatWithUser string
+	var chatMsg string
+
+	fmt.Println("online users:")
+	c.SearchOnlineUsers()
+
+	fmt.Println(">>>>>please select user to chat with, exit to exit:")
+	fmt.Scanln(&chatWithUser)
+
+	if strings.ToLower(chatWithUser) == "exit" {
+		return
+	} else if chatWithUser == c.Name {
+		fmt.Println("you can't chat with yourself")
+		return
+	}
+
+	fmt.Println(">>>>>please type in message, exit to exit:")
+	fmt.Scanln(&chatMsg)
+	if strings.ToLower(chatMsg) != "exit" {
+		return
+	}
+
+	for strings.ToLower(chatMsg) != "exit" {
+		if len(chatMsg) == 0 {
+			fmt.Println("message can't be empty")
+			continue
+		}
+
+		sendMsg := "to|" + chatWithUser + "|" + chatMsg + "\n"
+		_, err := c.conn.Write([]byte(sendMsg))
+		if err != nil {
+			fmt.Println("raise from Client.PrivateChat | conn.Write error:", err)
+			return
+		}
+
+		chatMsg = ""
+		fmt.Println(">>>>>please type in message, exit to exit:")
+		fmt.Scanln(&chatMsg)
 	}
 }
 
@@ -85,6 +187,8 @@ func main() {
 		return
 	}
 	fmt.Println("=====>client has been created<=====")
+
+	go client.DealResponse()
 
 	client.Run()
 }
