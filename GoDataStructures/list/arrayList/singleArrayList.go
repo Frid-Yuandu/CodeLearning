61df279bb97fb24f15c55d6d082f7113b854cb6e
yuandu <friedrich_wong@outlook.com>
2023-07-22 18:35:43 +0800

    Version:1.0 - Implement single list interface and other methods.

--- GoDataStructures/list/arrayList/singleArrayList.go
+++ GoDataStructures/list/arrayList/singleArrayList.go
@@ -1,76 +1,143 @@
 package arraylist
 
+import (
+	"fmt"
+	"yo/list"
+)
+
 const oneByte = 8
 
-type List[T any] struct {
+type SingleArrayList[T comparable] struct {
 	data []T
 	len  uint
 }
 
-func (l *List[T]) Size() uint  { return l.len }
-func (l *List[T]) Empty() bool { return l.len == 0 }
+func (l *SingleArrayList[T]) Len() uint     { return l.len }
+func (l *SingleArrayList[T]) IsEmpty() bool { return l.len == 0 }
 
-func (l *List[T]) init() *List[T] {
+func (l *SingleArrayList[T]) init() *SingleArrayList[T] {
 	l.data = make([]T, 0, oneByte)
 	l.len = 0
 	return l
 }
 
-func (l *List[T]) lazyInit() {
+func (l *SingleArrayList[T]) lazyInit() {
 	if l.data == nil {
 		l.init()
 	}
 }
 
-func (l *List[T]) AddAtHead(value T) {
+func (l *SingleArrayList[T]) Clear() {
+	l.init()
+}
+
+func (l *SingleArrayList[T]) Clone() list.SingleList[T] {
+	return &SingleArrayList[T]{
+		data: l.data,
+		len:  l.len,
+	}
+}
+
+func (l *SingleArrayList[T]) AddAtHead(value T) {
 	l.lazyInit()
-	l.data = append([]T{value}, l.data...)
+	l.add(value, []T{}, l.data)
+}
+
+func (l *SingleArrayList[T]) AddAtTail(value T) {
+	l.lazyInit()
+	l.add(value, l.data)
+}
+
+func (l *SingleArrayList[T]) AddAt(index uint, value T) {
+	l.lazyInit()
+	if index > l.len {
+		return
+	} else if l.IsEmpty() || index == l.len {
+		l.add(value, l.data)
+	} else {
+		l.add(value, l.data[:index-1], l.data[index-1:])
+	}
+}
+
+// the first element of joint is the head, and the second element is the tail.
+func (l *SingleArrayList[T]) add(value T, joint ...[]T) {
+	result := append(joint[0], value)
+	if len(joint) > 1 {
+		result = append(result, joint[1]...)
+	}
+	l.data = result
 	l.len++
 }
 
-func (l *List[T]) AddAtTail(value T) {
+func (l *SingleArrayList[T]) find(value T) (uint, bool) {
+	for i := uint(0); i < l.len; i++ {
+		if l.data[i] == value {
+			return i, true
+		}
+	}
+	return 0, false
+}
+
+func (l *SingleArrayList[T]) Find(value T) (uint, bool) {
+	return l.find(value)
+}
+
+func (l *SingleArrayList[T]) Get(index uint) (value T, found bool) {
+	if l.IsEmpty() || index >= l.len {
+		return
+	}
+	return l.data[index], true
+}
+
+func (l *SingleArrayList[T]) Head() (value T, found bool) {
 	l.lazyInit()
-	l.data = append(l.data, value)
-	l.len++
+	return l.Get(0)
 }
 
-func (l *List[T]) AddAtIndex(index uint, value T) {
-	// TODO: implement me
-	panic("implement me")
+func (l *SingleArrayList[T]) Tail() (value T, found bool) {
+	l.lazyInit()
+	return l.Get(l.len - 1)
 }
 
-func (l *List[T]) find(value T) uint {
-	// TODO: implement me
-	panic("implement me")
-}
-
-func (l *List[T]) RemoveByIndex(index uint) {
-	// TODO: implement me
-	panic("implement me")
-}
-
-func (l *List[T]) RemoveByValue(index uint) {
-	// TODO: implement me
-	panic("implement me")
-}
-
-func (l *List[T]) Head() (value T, found bool) {
-	if l.Empty() {
-		l.init()
-		return value, false
+func (l *SingleArrayList[T]) Set(index uint, value T) bool {
+	if _, ok := l.Get(index); !ok {
+		return false
 	}
-	return l.data[0], true
+	l.data[index] = value
+	return true
 }
 
-func (l *List[T]) Tail() (value T, found bool) {
-	if l.Empty() {
-		l.init()
-		return value, false
+func (l *SingleArrayList[T]) Remove(index uint) (value T, found bool) {
+	if value, found = l.Get(index); !found {
+		return
 	}
-	return l.data[l.len-1], true
+	l.remove(index)
+	return
 }
 
-func (l *List[T]) Show() string {
-	// TODO: implement me
-	panic("implement me")
+func (l *SingleArrayList[T]) RemoveByValue(value T) (index uint, found bool) {
+	if index, found = l.find(value); !found {
+		return
+	}
+	l.remove(index)
+	return
+}
+
+func (l *SingleArrayList[T]) remove(index uint) {
+	if index == l.len-1 {
+		l.data = l.data[:index]
+	} else {
+		l.data = append(l.data[:index], l.data[index+1:]...)
+	}
+	l.len--
+}
+
+func (l *SingleArrayList[T]) Show() (showed string) {
+	if !l.IsEmpty() {
+		showed += fmt.Sprintf("%v", l.data)
+		for i := uint(1); i < l.len; i++ {
+			showed += fmt.Sprintf(" -> %v", l.data[i])
+		}
+	}
+	return
 }
